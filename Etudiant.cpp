a88ebe4680f87d2e9d6109497be48a172a11bc80
Medhaddadi <77499801+Medhaddadi@users.noreply.github.com>
2022-04-26 11:16:55 +0000

    new

---
+++ Etudiant.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mohamedElHaddadi on 4/26/2022.
+//
+
+#include "Etudiant.h"
---
+++ Etudiant.h
@@ -0,0 +1,14 @@
+//
+// Created by mohamedElHaddadi on 4/26/2022.
+//
+
+#ifndef DL_EXAM_ETUDIANT_H
+#define DL_EXAM_ETUDIANT_H
+
+
+class Etudiant {
+
+};
+
+
+#endif //DL_EXAM_ETUDIANT_H
